0db0180ec6fb699f03c865672a2f01210dfdafe4
Alfin89 <104522708+Alfin89@users.noreply.github.com>
2022-06-13 17:39:56 +0700

    program_penampung_data
    print('•••••♦•••••••••••••••••••••••••••••••••••••••••••••♦•••••')
    print(' \t \tPROGRAM PENAMPUNGAN DATA')
    print('•••••♦•••••••••••••••••••••••••••••••••••••••••••••♦•••••')
    
    hobi = []
    
    nama = input('Masukan Nama : ')
    alamat = input('Masukan Alamat : ')
    prodi = input('Prodi Yang dipilih : ')
    umur = input('Tetala : ')
    kelas = input('Kelas berapa : ')
    
    print('•••••♦•••••••••••••••••••••••••••••••••••••••••••••♦•••••')
    print(' \t \t\tDATA ANDA')
    print('•••••♦•••••••••••••••••••••••••••••••••••••••••••••♦•••••')
    
    list = ['Nama   :' + nama,'Alamat :' + alamat,'Prodi  :' + prodi,'Umur   :'+umur,'Kelas  :' +kelas ]
             
    for data in list:
    	print(data)
    
    
    print('•••••♦•••••••••••••••••••••••••••••••••••••••••••••♦•••••')
    print(' \t \tDATA ANDA TELAH TERTAMPUNG')
    print('•••••♦•••••••••••••••••••••••••••••••••••••••••••••♦•••••')

---
+++ program_penampung_data.py
@@ -0,0 +1,37 @@
+print('•••••♦•••••••••••••••••••••••••••••••••••••••••••••♦•••••')
+
+print(' \t \tPROGRAM PENAMPUNGAN DATA')
+
+print('•••••♦•••••••••••••••••••••••••••••••••••••••••••••♦•••••')
+
+hobi = []
+
+nama = input('Masukan Nama : ')
+
+alamat = input('Masukan Alamat : ')
+
+prodi = input('Prodi Yang dipilih : ')
+
+umur = input('Tetala : ')
+
+kelas = input('Kelas berapa : ')
+
+print('•••••♦•••••••••••••••••••••••••••••••••••••••••••••♦•••••')
+
+print(' \t \t\tDATA ANDA')
+
+print('•••••♦•••••••••••••••••••••••••••••••••••••••••••••♦•••••')
+
+list = ['Nama   :' + nama,'Alamat :' + alamat,'Prodi  :' + prodi,'Umur   :'+umur,'Kelas  :' +kelas ]
+
+         
+
+for data in list:
+
+	print(data)
+
+print('•••••♦•••••••••••••••••••••••••••••••••••••••••••••♦•••••')
+
+print(' \t \tDATA ANDA TELAH TERTAMPUNG')
+
+print('•••••♦•••••••••••••••••••••••••••••••••••••••••••••♦•••••')
