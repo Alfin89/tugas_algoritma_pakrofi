8c1cbbfb5276f280062c1dafc251e2203ed33eac
Alfin89 <104522708+Alfin89@users.noreply.github.com>
2022-06-13 17:44:27 +0700

    Create program_percabangan_dan_perulangan.py

---
+++ program_percabangan_dan_perulangan.py
@@ -0,0 +1,65 @@
+print('•••••♦•••••••••••••••••••••••••••••••••••••••••••••♦•••••\n')
+
+print('\t\tPROGARAM JADWAL HARIAN\n')
+
+print('•••••♦•••••••••••••••••••••••••••••••••••••••••••••♦•••••')
+
+print('\t\tPilih hari di bawah ini ')
+
+print('•••••♦•••••••••••••••••••••••••••••••••••••••••••••♦•••••')
+
+satu_minggu = [
+
+ ' • Senin',' • Selasa',' • Rabu',' • Kamis',' • Jumat',' • Sabtu',
+
+ ' • Minggu'
+
+]
+
+for hari7 in satu_minggu :
+
+	print(hari7)print('•••••♦•••••••••••••••••••••••••••••••••••••••••••••♦•••••')
+
+print('\t\tMasukan dengan huruf kecil')
+
+print('•••••♦•••••••••••••••••••••••••••••••••••••••••••••♦•••••')
+
+hari_ini = input("\t\tMasukan Hari :") 
+
+print('•••••♦•••••••••••••••••••••••••••••••••••••••••••••♦•••••')
+
+if(hari_ini == "senin"):
+
+    print("\t\tSaya akan kuliah")
+
+elif(hari_ini == "selasa"):
+
+    print("\t\tSaya akan kuliah")
+
+elif(hari_ini == "rabu"):
+
+    print("\t\tSaya akan kuliah")
+
+elif(hari_ini == "kamis"):
+
+    print("\t\tSaya akan kuliah")
+
+elif(hari_ini == "jumat"):
+
+    print("\t\tSaya ada Bootcamp")
+
+elif(hari_ini == "sabtu"):
+
+    print("\t\tSaya akan kuliah")
+
+elif(hari_ini == "minggu"):
+
+    print("\t\tSaya akan Healing •_°")
+
+else:
+
+    	print('\t\tHari yang anda masukan salah ')
+
+ 
+
+              
